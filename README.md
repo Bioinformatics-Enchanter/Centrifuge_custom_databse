c105a30a12e3715c094471fb6352531f1b38b968
Kiran <kiran.adhikari@ciandx.com>
2024-02-07 16:03:51 -0500

    Script for centrifuge custom database

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# Centrifuge_Pipeline
+# Creating Snakemake pipeline for metagenomics classifier tool: Centrifuge for Oxford Nanopore reads.
+
+- Centrifuge is microbial classification tools used for rapid and accurate labeling of reads and there classification. 
+- Centrifuge use Burrows-Wheeler transform (BWT) and the Ferragina-Manzini (FM) index method to classify microbial community. 
+- For classification of species usually alignment is not needed, therefore for classification, indxes are build with large set of target genomes. 
+- For this centrifuge version 1.0.4 is used to build indexes for Bacteria, Fungi, Virus and human gnomes. 
+
+# Building index for Bacteria, fungi, virues
+
+- To build index centrifuge use following files:
+    - taxonomy/name.dmp file 
+    - taxonomy/node.dmp file 
+    - library/input_sequences.fna
+    - seqid2taxid.map
+  
+- After building indexes it give outputs with suffixes .1.cf, 2..cf and so on. 
+
+
+
+
+
+
---
+++ edirect_script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+{
+    esearch -db taxonomy -query "Aspergillus fumigatus" | efetch -format docsum | xtract -pattern DocumentSummary -element ScientificName TaxId
+    esearch -db taxonomy -query "Aspergillus flavus" | efetch -format docsum | xtract -pattern DocumentSummary -element ScientificName TaxId
+    esearch -db taxonomy -query "Aspergillus niger" | efetch -format docsum | xtract -pattern DocumentSummary -element ScientificName TaxId
+    esearch -db taxonomy -query "Aspergillus terreus" | efetch -format docsum | xtract -pattern DocumentSummary -element ScientificName TaxId
+    esearch -db taxonomy -query "Candida albicans" | efetch -format docsum | xtract -pattern DocumentSummary -element ScientificName TaxId
+    esearch -db taxonomy -query "Candida auris" | efetch -format docsum | xtract -pattern DocumentSummary -element ScientificName TaxId
+    esearch -db taxonomy -query "Candida dubliniensis" | efetch -format docsum | xtract -pattern DocumentSummary -element ScientificName TaxId
+    esearch -db taxonomy -query "Candida glabrata" | efetch -format docsum | xtract -pattern DocumentSummary -element ScientificName TaxId
+    esearch -db taxonomy -query "Candida krusei" | efetch -format docsum | xtract -pattern DocumentSummary -element ScientificName TaxId
+    esearch -db taxonomy -query "Candida lusitaniae" | efetch -format docsum | xtract -pattern DocumentSummary -element ScientificName TaxId
+    esearch -db taxonomy -query "Candida parapsilosis" | efetch -format docsum | xtract -pattern DocumentSummary -element ScientificName TaxId
+    esearch -db taxonomy -query "Candida tropicalis" | efetch -format docsum | xtract -pattern DocumentSummary -element ScientificName TaxId
+    esearch -db taxonomy -query "Cryptococcus neoformans" | efetch -format docsum | xtract -pattern DocumentSummary -element ScientificName TaxId
+    esearch -db taxonomy -query "Encephalitozoon intestinalis" | efetch -format docsum | xtract -pattern DocumentSummary -element ScientificName TaxId
+    esearch -db taxonomy -query "Trichophyton rubrum" | efetch -format docsum | xtract -pattern DocumentSummary -element ScientificName TaxId
+    esearch -db taxonomy -query "Trichophyton mentagrophytes" | efetch -format docsum | xtract -pattern DocumentSummary -element ScientificName TaxId
+    esearch -db taxonomy -query "Staphylococcus aureus" | efetch -format docsum | xtract -pattern DocumentSummary -element ScientificName TaxId
+} > fungus_tax_ids.txt
+
+
+ 
